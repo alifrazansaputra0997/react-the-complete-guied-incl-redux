1b2c112409fd4dd71ee76a94f1a06ac0703e00b6
Alif Razan Saputra <alifrazan97@gmail.com>
2026-06-21 06:32:58 +0700

    variables

--- assets/scripts/utils.js
+++ assets/scripts/utils.js
@@ -1,3 +1,8 @@
 export const apikey = "bbbb";
 
 export const apikey2 = "cccc";
+
+// you could use $ and _ symbols to naming your variables
+let $checkname = "helloworld"
+let _checkname = "helloworld"
+
