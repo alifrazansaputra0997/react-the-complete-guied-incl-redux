102c1a03bc9908275b060b311a6e6c5e09717cd6
Alif Razan Saputra <alifrazan97@gmail.com>
2026-06-21 06:51:10 +0700

    arrow func, shorter func

--- assets/scripts/utils.js
+++ assets/scripts/utils.js
@@ -6,3 +6,31 @@ export const apikey2 = "cccc";
 let $checkname = "helloworld"
 let _checkname = "helloworld"
 
+//  Code Excercise: Working with Functions
+combine(a,b,c){
+    return a * b / c;
+}
+
+console.log('CALCULATE 1,2,3 = ', combine(1,2,3))
+
+// arrow function
+// some of the function doesnt need a name so you could leave it blank and use the arrow function
+export default (param1, param2) => {
+
+}
+() =>{
+    console.log('xx')
+}
+(param1,param2) => {
+
+}
+
+// instead of
+number => {
+    return number * 3;
+}
+// you could write
+numberx => numberx * 3;
+
+// returning object
+numbery => ({ age: numbery })
